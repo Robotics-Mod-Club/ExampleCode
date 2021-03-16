0670c13d08da5de0e62473caa0c3953def3b3816
Anna <anna.pedersen@student.education.wa.edu.au>
2021-03-16 11:37:58 +0800

    Led example on pin 9

---
+++ LED_Code/LED_Code.ino
@@ -0,0 +1,23 @@
+
+/*
+ * Set all your variables here
+ */
+int LedPin = 9; //set which pin the LED is connected to
+
+/*
+ * The setup function is called once at the start when the code runs
+ * In here put up all your setup features like setting your pinMode
+ */
+void setup() {
+  pinMode(LedPin, OUTPUT);
+}
+
+/*
+ * The loop function runs over and over again
+ */
+void loop() {
+  digitalWrite(LedPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LedPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
